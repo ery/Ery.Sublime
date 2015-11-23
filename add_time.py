7438982aa205fdd09f0cb18bc62fa6759a223acc
Ery <erylei@gmail.com>
2015-11-23 10:47:42 +0800

    update plugins

--- add_time.py
+++ add_time.py
@@ -2,8 +2,7 @@ import sublime, sublime_plugin, datetime
 
 class AddTimeCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.run_command("insert_snippet",
-      {
-        "contents": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +"\n"
+    self.view.run_command("insert_snippet", {
+        "contents": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
       }
     )
